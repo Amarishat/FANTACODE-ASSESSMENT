3a1cac7edc77bdf9510470d34208e8662e5b082f
Amarishat <96168541+Amarishat@users.noreply.github.com>
2022-08-25 08:21:03 +0530

    Add files via upload
    HelloWorld-.net
    Day, Month-C#
    HTML,CSS-TABLE Json

---
+++ HelloWorld.net/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+// var name = "Amarish";
+// Console.WriteLine("Hello " + name + "!");
+// var name = "Amarish";
+// Console.WriteLine($"Hello {name.ToUpper()}!");
+var names = new[] { "Amarish", "Felipe", "Emillia" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
---
+++ MyAppC#DM/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+    using System;  
+      
+    namespace DateTimeFormatInCSharpSample  
+    {  
+        class Program  
+        {  
+            static void Main(string[] args)  
+            {  
+                
+                DateTime aDate = DateTime.Now;  
+                
+                Console.WriteLine(aDate.ToString("dddd MMMM"));  
+      
+                Console.ReadKey();  
+            }  
+        }  
+    }  
